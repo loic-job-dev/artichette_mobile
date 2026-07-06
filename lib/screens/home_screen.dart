cad276fa55af7c2edc289e5bc4d7353d44773469
loic-job-dev <loicjobard@hotmail.com>
2026-07-06 14:49:21 +0200

    added GoRouter class, and a navigation exemple in HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:artichette/widgets/login_form.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../view_models/booking_view_model.dart';
@@ -20,7 +21,6 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
-          LoginForm(),
           StaySearchCard(
             onSearch: (start, end, adults, children) {
               vm.searchRooms(
@@ -46,8 +46,14 @@ class HomeScreen extends StatelessWidget {
                   roomType: room,
                   onBook: (room) {
                     vm.createBooking(room);
+                    //Future navigation
+                    //context.go('/booking');
                   },
                   onDetails: () {},
+                  //TODO : Add room details page
+                  // onDetails: (room) {
+                  //   context.go('/roomdetails');
+                  // },
                 ),
               ),
             ),
