2713f9d1d7ecef5b2fbb6572191158542ea9b35d
K-pujol <killianpujol26@gmail.com>
2026-07-07 11:43:41 +0200

    fix du scroll

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -24,33 +24,39 @@ class _AuthScreenState extends State<AuthScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SegmentedButton<AuthMode>(
-          segments: const [
-            ButtonSegment(value: AuthMode.login, label: Text('Connexion')),
-            ButtonSegment(value: AuthMode.register, label: Text("Inscription")),
-          ],
-          selected: {_selected},
-          onSelectionChanged: (value) {
-            setState(() {
-              _selected = value.first;
-            });
-          },
-        ),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          SegmentedButton<AuthMode>(
+            segments: const [
+              ButtonSegment(value: AuthMode.login, label: Text('Connexion')),
+              ButtonSegment(
+                value: AuthMode.register,
+                label: Text("Inscription"),
+              ),
+            ],
+            selected: {_selected},
+            onSelectionChanged: (value) {
+              setState(() {
+                _selected = value.first;
+              });
+            },
+          ),
 
-        const SizedBox(height: 24),
+          const SizedBox(height: 24),
 
-        AnimatedSwitcher(
-          duration: const Duration(milliseconds: 250),
-          transitionBuilder: (child, animation) {
-            return FadeTransition(opacity: animation, child: child);
-          },
-          child: _selected == AuthMode.login
-              ? const LoginForm(key: ValueKey('login'))
-              : const SignupForm(key: ValueKey('register')),
-        ),
-      ],
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 250),
+            transitionBuilder: (child, animation) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+            child: _selected == AuthMode.login
+                ? const LoginForm(key: ValueKey('login'))
+                : const SignupForm(key: ValueKey('register')),
+          ),
+        ],
+      ),
     );
   }
 }
